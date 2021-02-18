43d0339eb082c53bcf7481aea63cfb546c6c7be1
Maya Louise <mayalouised@gmail.com>
2021-02-18 16:31:20 -0600

    completed routes, controllers and views without styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drink_serialized = open(url).read
+drink = JSON.parse(drink_serialized)
+ingredients = drink["drinks"]
+
+ingredients.each do |ingredient|
+  drink_ingredient = ingredient["strIngredient1"]
+  new_ingredient = Ingredient.create(name: drink_ingredient)
+  p "Created new ingredient #{new_ingredient}"
+end
